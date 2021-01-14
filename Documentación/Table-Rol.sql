4a8d38890ae74bd825a3a2c0fb63a9f732aec94d
Ale Quesada <ale23200@gmail.com>
2021-01-14 17:21:06 -0600

    creation of the 'usuario_rol' table

--- Documentación/Table-Rol.sql
+++ Documentación/Table-Rol.sql
@@ -1,7 +1,6 @@
-CREATE TABLE `Rol` (
+CREATE TABLE `rol` (
   `pk_id_rol` int(11) NOT NULL AUTO_INCREMENT,
   `tc_nombre_rol` varchar(30) NOT NULL,
-  `tc_borrrado` bit(1) NOT NULL,
-  PRIMARY KEY (`pk_id_rol`),
-  UNIQUE KEY `pk_id_rol_UNIQUE` (`pk_id_rol`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `tc_borrado` bit(1) NOT NULL DEFAULT b'0',
+  PRIMARY KEY (`pk_id_rol`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
