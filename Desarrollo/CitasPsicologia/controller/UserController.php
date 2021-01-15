467511889874d24919e45af4e6d75288f4e7321e
Emily Melendez <emi.mci.95@gmail.com>
2021-01-15 14:30:44 -0600

    iniciar session button function

--- Desarrollo/CitasPsicologia/controller/UserController.php
+++ Desarrollo/CitasPsicologia/controller/UserController.php
@@ -9,7 +9,6 @@ class UserController {
         $this->view = new View();
     }
 
-    //PROVIDER
 
     public function showLoginView() {
         $this->view->show("loginView.php", null);
@@ -18,6 +17,7 @@ class UserController {
     public function showIndexView() {
         $this->view->show("indexView.php", null);
     }
+
     public function logIn() {
         require 'model/UserModel.php';
 
@@ -27,26 +27,23 @@ class UserController {
         if (empty($identification) || empty($password)) {
             echo 'error';
         } else {
-            $user =  UserModel::singleton();
+            $user = UserModel::singleton();
             $result = $user->loginCheck($identification, $password);
             if ($result != null) {
 
 
-                if($result == 1){//admi
+                if ($result == 1) {//admi
                     echo '1';
                     //$_SESSION['user'][0] = $identification;
-                }elseif($result == 2){//profesional
+                } elseif ($result == 2) {//profesional
                     echo '2';
                     //$_SESSION['user'][0] = $identification;
-                }else{
+                } else {
                     echo '3';
                 }
-               
             } else {
                 echo 'error';
             }
-        
-
         }
     }
 
@@ -61,53 +58,6 @@ class UserController {
         $products['products'] = $product->show_all_products();
         print_r($products);
 //        $this->view->show("startingMain.php", null);
-    }
-
-    public function showSearchProducts() {
-        $this->view->show("searchProducts.php", null);
-    }
-
-    public function searchProducts() {
-        require 'model/ProductoModel.php';
-        $product = ProductoModel::singleton();
-        $products['searchedProducts'] = $product->search_products($_POST['name']);
-        $this->view->show("listSearchedProducts.php", $products);
-    }
-
-    public function showDeleteProduct() {
-        $id = $_GET['id'];
-        require 'model/ProductoModel.php';
-        $products = ProductoModel::singleton();
-        $product['productByCode'] = $products->search_product_by_id($id);
-        $this->view->show("deleteProduct.php", $product);
-    }
-
-    public function deleteProduct() {
-        $id = $_GET['id'];
-        require 'model/ProductoModel.php';
-        $products = ProductoModel::singleton();
-        $products->delete_product($id);
-        $product['productByCode'] = $products->search_product_by_id($id);
-        $this->view->show("deleteProduct.php", $product);
-    }
-
-    public function showUpdateProduct() {
-        $id = $_GET['id'];
-        require 'model/ProductoModel.php';
-        $product = ProductoModel::singleton();
-        $productToUpdate['product'] = $product->search_product_by_id($id);
-        $this->view->show("updateProduct.php", $productToUpdate);
-    }
-
-    public function update_product() {
-        require 'model/ProductoModel.php';
-        $product = ProductoModel::singleton();
-        $product->updateProduct($_POST['id'], $_POST['name'], $_POST['price'], $_POST['description'], $_POST['quantity']);
-        echo 'Producto actualizado';
-    }
-
-    public function showSynchronization() {
-        $this->view->show("synchronization.php", null);
     }
 
 }
--- Desarrollo/CitasPsicologia/model/AppointmentModel.php
+++ Desarrollo/CitasPsicologia/model/AppointmentModel.php
@@ -23,52 +23,6 @@ class AppointmentModel {
         return self::$instance;
     }
 
-    //PROVIDER ONE
-    public function register_product($name, $price, $description, $quantity, $image) {
-        $consulta = $this->db->prepare("call sp_insert_product('" . $name . "', " . $price . ", '" . $description . "'," . $this->providerID . "," . $quantity . ",'" . $image . "')");
-        $consulta->execute();
-        $resultado = $consulta->fetchAll();
-        $consulta->closeCursor();
-        return $resultado;
-    }
-
-    public function show_all_products() {
-        $consulta = $this->db->prepare("call  sp_get_products(" . $this->providerID . ")");
-        $consulta->execute();
-        $resultado = $consulta->fetchAll();
-        $consulta->closeCursor();
-        return $resultado;
-    }
-
-    public function search_products($name) {
-        $consulta = $this->db->prepare("call  sp_search_products_by_name('" . $name . "'," . $this->providerID . ")");
-        $consulta->execute();
-        $resultado = $consulta->fetchAll();
-        $consulta->closeCursor();
-        return $resultado;
-    }
-
-    public function search_product_by_id($id) {
-        $consulta = $this->db->prepare("call sp_search_product_by_id(" . $id . "," . $this->providerID . ")");
-        $consulta->execute();
-        $resultado = $consulta->fetchAll();
-        $consulta->closeCursor();
-        return $resultado;
-    }
-
-    public function delete_product($id) {
-        $consulta = $this->db->prepare("call sp_delete_product_by_id(" . $id . "," . $this->providerID . ")");
-        $consulta->execute();
-        $resultado = $consulta->fetchAll();
-        $consulta->closeCursor();
-        return $resultado;
-    }
-
-    public function updateProduct($id, $name, $price, $description, $quantity) {
-        $consulta = $this->db->prepare("call sp_update_product(" . $id . ", '" . $name . "'," . $price . ", '" . $description . "'," . $quantity . "," . $this->providerID . ")");
-        $consulta->execute();
-    }
-
 }
 ?>
 
