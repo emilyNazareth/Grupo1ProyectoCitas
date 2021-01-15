73df78ea070c53233f252a291faf449566ed3b89
Emily Melendez <emi.mci.95@gmail.com>
2021-01-15 14:36:26 -0600

    consulta de citas button function

--- Desarrollo/CitasPsicologia/view/indexView.php
+++ Desarrollo/CitasPsicologia/view/indexView.php
@@ -9,9 +9,9 @@ include 'public/header.php';
         <div class="row homeOptions">
             <a class="btn btn-success col-md-5 btn-block" href="?controlador=User&accion=showLoginView">Iniciar Sesión</a>
         </div>
-<!--        <div class="row homeOptions">
+        <div class="row homeOptions">
             <a class="btn btn-success col-md-5 btn-block" href="?controlador=Appointment&accion=showCheckHistory">Consulta de citas</a>
-        </div>-->
+        </div>
         <div class="row homeOptions">
             <div class="card">
                 <div id="instructions" class="card-header">
