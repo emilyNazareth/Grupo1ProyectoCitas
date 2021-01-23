c4c8eef9b7196bcd11ac675202a664998cf8676d
YerlinLeal <yerlin.leal@ucr.ac.cr>
2021-01-22 22:09:12 -0600

    Add validations in form registerProfessional

--- Desarrollo/CitasPsicologia/public/header.php
+++ Desarrollo/CitasPsicologia/public/header.php
@@ -5,7 +5,6 @@
         <title>Citas en linea</title>
         <meta charset="utf-8"/>
         <meta name="description" content="Citas de psicologia" />
-        <meta name="viewport" content="width=device-width,initial-scale=1"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
@@ -14,7 +13,7 @@
 
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-        <link rel="stylesheet" href="public\css\style.css">
+        <link rel="stylesheet" href="public/css/style.css">
         
         <script src="public/js/jquery-3.2.1.js" type="text/javascript"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
@@ -22,16 +21,37 @@
 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-        <!--DATEPICKER-->
-        <script type="text/javascript" src="libs\bootstrap-datepicker\js\bootstrap-datepicker.js"></script>
         <script src="public/js/script.js" type="text/javascript"></script>
         <script src="public/js/Site.js"></script>
         
-        <link rel="stylesheet" type="text/css" href="libs\bootstrap-datepicker\css\bootstrap-datepicker.css">
-        <link rel="stylesheet" type="text/css" href="public/css/style.css"/>
-        <script src="public/js/script.js" type="text/javascript"></script>
+        <!-- For validations -->
+        <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
+        <script>
+        jQuery.extend(jQuery.validator.messages, {
+            required: "*",
+            remote: "Please fix this field.",
+            email: "Please enter a valid email address.",
+            url: "Please enter a valid URL.",
+            date: "Please enter a valid date.",
+            dateISO: "Please enter a valid date (ISO).",
+            number: "Please enter a valid number.",
+            digits: "Please enter only digits.",
+            creditcard: "Please enter a valid credit card number.",
+            equalTo: "Please enter the same value again.",
+            accept: "Please enter a value with a valid extension.",
+            maxlength: jQuery.validator.format("Please enter no more than {0} characters."),
+            minlength: jQuery.validator.format("Please enter at least {0} characters."),
+            rangelength: jQuery.validator.format("Please enter a value between {0} and {1} characters long."),
+            range: jQuery.validator.format("Please enter a value between {0} and {1}."),
+            max: jQuery.validator.format("Please enter a value less than or equal to {0}."),
+            min: jQuery.validator.format("Please enter a value greater than or equal to {0}.")
+        });
+        </script>
+        <!-- For validations -->
+       
     </head>
     <body class="title">
+        <div id="resultado"></div>
 
 
 
--- Desarrollo/CitasPsicologia/view/RegisterProfessionalView.php
+++ Desarrollo/CitasPsicologia/view/RegisterProfessionalView.php
@@ -2,7 +2,7 @@
 include 'public/header.php';
 ?>
 
-<form>
+<form id="form-professional-register">
     
     <div class="container text-left">
         
@@ -14,55 +14,55 @@ include 'public/header.php';
                 <!--IDENTIFICACION-->
                 <div class="row professional-register">
                     <label class="col-sm-4 control-label small" for="identification">N&uacute;mero de identificaci&oacute;n:</label>
-                    <input class="col-sm-6 form-control form-control-sm" type="number" id="identification" name="identification" required>
+                    <input class="col-sm-6 form-control form-control-sm" type="number" id="identification" name="identification" required />
                 </div>
 
                 <!--CONTRASENA-->
                 <div class="row professional-register">
                     <label class="col-sm-4 control-label small" for="name">Contrase&ntilde;a:</label>
-                    <input class="col-sm-6 form-control form-control-sm" type="password" id="password" name="password">
+                    <input class="col-sm-6 form-control form-control-sm" type="password" id="password" name="password" required />
                 </div>
 
                 <!--NOMBRE-->
                 <div class="row professional-register">
                     <label class="col-sm-4 control-label small" for="name">Nombre:</label>
-                    <input class="col-sm-6 form-control form-control-sm" type="text" id="name" name="name">
+                    <input class="col-sm-6 form-control form-control-sm" type="text" id="name" name="name" required />
                 </div>
 
                 <!--PRIMER APELLIDO-->
                 <div class="row professional-register">
-                    <label class="col-sm-4 control-label small" for="firstLastName">Primer Apellido:</label>
-                    <input class="col-sm-6 form-control form-control-sm" type="text" id="firstLastName" name="firstLastName">
+                    <label class="col-sm-4 control-label small" for="firstLastName">Primer apellido:</label>
+                    <input class="col-sm-6 form-control form-control-sm" type="text" id="firstLastName" name="firstLastName" required />
                 </div>
 
                 <!--SEGUNDO APELLIDO-->
                 <div class="row professional-register">
-                    <label class="col-sm-4 control-label small" for="secondLastName">Segundo Apellido:</label>
-                    <input class="col-sm-6 form-control form-control-sm" type="text" id="secondLastName" name="secondLastName">
+                    <label class="col-sm-4 control-label small" for="secondLastName">Segundo apellido:</label>
+                    <input class="col-sm-6 form-control form-control-sm" type="text" id="secondLastName" name="secondLastName" required />
                 </div>
 
                 <!--TELEFONO PERSONAL-->
                 <div class="row professional-register">
-                    <label class="col-sm-4 control-label small" for="personalPhone">Tel&eacute;fono Personal:</label>
-                    <input class="col-sm-6 form-control form-control-sm" type="number" id="personalPhone" name="personalPhone">
+                    <label class="col-sm-4 control-label small" for="personalPhone">Tel&eacute;fono personal:</label>
+                    <input class="col-sm-6 form-control form-control-sm" type="number" id="personalPhone" name="personalPhone" required />
                 </div>
 
                 <!--TELEFONO DE HABITACION-->
                 <div class="row professional-register">
                     <label class="col-sm-4 control-label small" for="roomPhone">Tel&eacutefono de habitaci&oacuten:</label>
-                    <input class="col-sm-6 form-control form-control-sm" type="number" id="roomPhone" name="roomPhone">
+                    <input class="col-sm-6 form-control form-control-sm" type="number" id="roomPhone" name="roomPhone" />
                 </div>
 
                 <!--FECHA DE NACIMIENTO-->
                 <div class="row professional-register">
-                    <label class="col-sm-4 control-label small" for="birthday">Fecha de nacimiento</label>
-                    <input type="date" class="col-sm-6 form-control form-control-sm" id="birthday" name="birthday" />
+                    <label class="col-sm-4 control-label small" for="birthday">Fecha de nacimiento:</label>
+                    <input type="date" class="col-sm-6 form-control form-control-sm" id="birthday" name="birthday" required />
                 </div>
 
                 <!--SEXO-->
                 <div class="row professional-register">
-                    <label class="col-sm-4 control-label small" for="gender">Sexo</label>
-                    <select class="col-sm-6 custom-select custom-select-sm" id="gender" name="gender">
+                    <label class="col-sm-4 control-label small" for="gender">Sexo:</label>
+                    <select class="col-sm-6 custom-select custom-select-sm" id="gender" name="gender" required />
                         <option value="M">Masculino</option>
                         <option value="F">Femenino</option>
                     </select>
@@ -71,7 +71,7 @@ include 'public/header.php';
                 <!--ESTADO CIVIL-->
                 <div class="row professional-register">
                     <label class="col-sm-4 control-label small" for="civilStatus">Estado civil:</label>
-                    <select class="col-sm-6 custom-select custom-select-sm" id="civilStatus" name="civilStatus">
+                    <select class="col-sm-6 custom-select custom-select-sm" id="civilStatus" name="civilStatus" required />
                         <option value="Casado(a)">Casado(a)</option>
                         <option value="Divorsiado(a)">Divorciado(a)</option>
                         <option value="Viudo(a)">Viudo(a)</option>
@@ -81,8 +81,8 @@ include 'public/header.php';
 
                 <!--NUMERO DE PLAZA-->
                 <div class="row professional-register">
-                    <label class="col-sm-4 control-label small" for="placeNumber">N&uacutemero de plaza</label>
-                    <input class="col-sm-6 form-control form-control-sm" type="number" id="placeNumber" name="placeNumber">
+                    <label class="col-sm-4 control-label small" for="placeNumber">N&uacutemero de plaza:</label>
+                    <input class="col-sm-6 form-control form-control-sm" type="number" id="placeNumber" name="placeNumber" />
                 </div>
 
             </div>
@@ -91,8 +91,8 @@ include 'public/header.php';
 
                 <!--ESTADO-->
                 <div class="row professional-register" id="state">
-                    <label class="col-sm-4 control-label small" for="status">Estado</label>
-                    <select class="col-sm-6 custom-select custom-select-sm" id="status" name="status">
+                    <label class="col-sm-4 control-label small" for="status">Estado:</label>
+                    <select class="col-sm-6 custom-select custom-select-sm" id="status" name="status" required />
                         <option id="active" value="1">Activo</option>
                         <option id="inactive" value="0">Inactivo</option>
                     </select>
@@ -101,19 +101,19 @@ include 'public/header.php';
                 <!--CONTACTO EMERGENCIA-->
                 <div class="row professional-register">
                     <label class="col-sm-4 control-label small" for="emergencyContactName">Contacto en caso de emergencia:</label>
-                    <input class="col-sm-6 form-control form-control-sm" type="text" id="emergencyContactName" name="emergencyContactName">
+                    <input class="col-sm-6 form-control form-control-sm" type="text" id="emergencyContactName" name="emergencyContactName" required />
                 </div>
 
                 <!--NUM CONTACTO EMERGENCIA-->
                 <div class="row professional-register">
                     <label class="col-sm-4 control-label small" for="emergencyContactNumber">N&uacutemero del contacto:</label>
-                    <input class="col-sm-6 form-control form-control-sm" type="number" id="emergencyContactNumber" name="emergencyContactNumber">
+                    <input class="col-sm-6 form-control form-control-sm" type="number" id="emergencyContactNumber" name="emergencyContactNumber" required />
                 </div>
 
                 <!--ESCOLARIDAD-->
                 <div class="row professional-register">
                     <label class="col-sm-4 control-label small" for="scholarship">Escolaridad:</label>
-                    <select class="col-sm-6 custom-select custom-select-sm" id="scholarship" name="scholarship">
+                    <select class="col-sm-6 custom-select custom-select-sm" id="scholarship" name="scholarship" required>
                         <option value="T&eacutecnico">T&eacutecnico</option>
                         <option value="Bachillerato">Bachillerato</option>
                         <option value="Licenciatura">Licenciatura</option>
@@ -136,64 +136,63 @@ include 'public/header.php';
 
                 <!--PROVINCIA-->
                 <div class="row professional-register">
-                    <label class="col-sm-4 control-label small" for="province">Provincia</label>
-                    <select class="col-sm-6 custom-select custom-select-sm" id="province" name="province" onchange="activeCanton(); return false;">
-                        <option selected>Seleccione...</option>
+                    <label class="col-sm-4 control-label small" for="province">Provincia:</label>
+                    <select class="col-sm-6 custom-select custom-select-sm" id="province" name="province" required onchange="activeCanton(); return false;">
+                        <option disabled selected value>Seleccione...</option>
                     </select>
                 </div>
 
                 <!--CANTON-->
                 <div class="row professional-register">
-                    <label class="col-sm-4 control-label small" for="canton">Cant&oacuten</label>
-                    <select class="col-sm-6 custom-select custom-select-sm" id="canton" name="canton" onchange="activeDistrict(); return false;"></select>
+                    <label class="col-sm-4 control-label small" for="canton">Cant&oacuten:</label>
+                    <select class="col-sm-6 custom-select custom-select-sm" id="canton" name="canton" required onchange="activeDistrict(); return false;" required></select>
                 </div>
 
                 <!--DISTRITO-->
                 <div class="row professional-register">
-                    <label class="col-sm-4 control-label small" for="district">Distrito</label>
-                    <select class="col-sm-6 custom-select custom-select-sm" id="district" name="district"></select>
+                    <label class="col-sm-4 control-label small" for="district">Distrito:</label>
+                    <select class="col-sm-6 custom-select custom-select-sm" id="district" name="district" required></select>
                 </div>
 
                 <!--DIRECCION-->
                 <div class="row professional-register">
                     <label class="col-sm-4 control-label small" for="address">Direcci&oacuten:</label>
-                    <textarea class="col-sm-6 form-control form-control-sm" id="address" name="address" rows="1"></textarea>
+                    <textarea class="col-sm-6 form-control form-control-sm" id="address" name="address" rows="1" required></textarea>
                 </div>
 
             </div>
         
             <div class="col-sm-12 buttons-professional-register">
                 <div class="row">
+                    
                     <!--BT ATRAS-->
                     <div class="col-sm-6">
-                        <a onclick="registerProfessional()" class="btn btn-success btn-sm">Atr&aacute;s</a>
+                        <a class="btn btn-success btn-sm" href="?controlador=User&accion=showAdministratorMainView">Atr&aacute;s</a>
                     </div>
 
-                    <!--BT ACEPTAR-->
+                    
                     <div class="col-sm-6">
                         <div class="row">
                             <div class="col-sm-4"></div>
                         
                             <div class="col-sm-6 btn-action">
-                                <button type="button" onclick="registerProfessional()" class="btn btn-success btn-sm">Aceptar</button>
+                                <!--BT ACEPTAR-->
+                                <input type="button" onclick="registerProfessional()" class="btn btn-success btn-sm" id="btn-accept" value="Aceptar"/>
 
-                            <!--BT CANCELAR-->
-                            <a onclick="" class="btn btn-success btn-sm" id="btn-cancel">Cancelar</a>
+                                <!--BT CANCELAR-->
+                                <button type="button" onclick="cleanFormRegisterProfessional()" class="btn btn-success btn-sm" id="btn-cancel">Cancelar</button>
+                            </div>
                         </div>
-                        </div>
-                        
-                        
+
                     </div>
                 </div>
             </div>
-            
+
         </div>
         
     </div>
 
 </form>
-
-<script src="public\js\Site.js"></script>
 
 <script>
     window.onload = function () {
