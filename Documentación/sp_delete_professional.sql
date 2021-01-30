ce6feb95b3e8ea6599c84dca3fc25bdcb1003625
YerlinLeal <yerlin.leal@ucr.ac.cr>
2021-01-30 13:35:13 -0600

    sp delete professional

--- Documentación/sp_delete_professional.sql
+++ Documentación/sp_delete_professional.sql
@@ -7,6 +7,8 @@ USE `controlcitas`$$
 CREATE DEFINER=`controlcitas`@`%` PROCEDURE `sp_delete_professional`(cedula integer)
 BEGIN
 	DELETE FROM usuario WHERE pk_cedula_usuario = cedula;
+	DELETE FROM usuario_rol WHERE fk_id_usuario = cedula;
+
     SELECT row_count() as result;
 END$$
 
