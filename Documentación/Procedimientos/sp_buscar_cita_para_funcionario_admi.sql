c702bd5e0bfae659e95b4cd85ccdcb6e63e1c8b0
Emily Melendez <emi.mci.95@gmail.com>
2021-02-03 11:54:20 -0600

    update procedure to search appointments

--- Documentación/Procedimientos/sp_buscar_cita_para_funcionario_admi.sql
+++ Documentación/Procedimientos/sp_buscar_cita_para_funcionario_admi.sql
@@ -1,18 +1,15 @@
-
 DELIMITER $$
 DROP PROCEDURE IF EXISTS sp_buscar_cita_para_funcionario_admi;
 CREATE procedure sp_buscar_cita_para_funcionario_admi
-(id_funcionario_ int(11),
-id_cita_ int(11),
-fecha_inicial_ date,
-fecha_final_ date,
-id_profesional_ int(11),
+(id_funcionario_ varchar(11),
+id_cita_ varchar(11),
+fecha_inicial_ varchar(11),
+fecha_final_ varchar(11),
+id_profesional_ varchar(11),
 sexo_ char(1))
 
 begin
-
-
-		Select C.pk_id_cita,
+	Select C.pk_id_cita,
 		U.tc_nombre,
 		U.tc_primer_apellido,
 		U.tc_segundo_apellido,
@@ -22,20 +19,21 @@ begin
 		C.tc_hora
 	FROM usuario U,
 		 cita C,
-         rol R,
 		 usuario_rol UR	
-	 WHERE					
-			(C.tc_borrado = 0 and U.pk_cedula_usuario = id_funcionario_ and C.fk_id_funcionario = id_funcionario_		                        
-			and R.tc_nombre_rol = 'Funcionario'  and UR.fk_id_rol = 3 and UR.fk_id_usuario = id_funcionario_) 
-            or (C.tf_fecha >= fecha_inicial_ and C.tf_fecha <= fecha_final_ and   R.tc_nombre_rol = 'Funcionario'  and C.tc_borrado = 0 
-            and UR.fk_id_rol = 3 and UR.fk_id_usuario = U.pk_cedula_usuario and C.fk_id_funcionario = U.pk_cedula_usuario)	           
-			or (U.tc_sexo = sexo_ and  R.tc_nombre_rol = 'Funcionario'  and C.tc_borrado = 0 
-            and UR.fk_id_rol = 3 and UR.fk_id_usuario = U.pk_cedula_usuario and C.fk_id_funcionario = U.pk_cedula_usuario)
-            or (C.pk_id_cita = id_cita_ and  R.tc_nombre_rol = 'Funcionario'  and C.tc_borrado = 0 
-            and UR.fk_id_rol = 3 and UR.fk_id_usuario = U.pk_cedula_usuario and C.fk_id_funcionario = U.pk_cedula_usuario)
-			or (C.fk_id_profesional = id_profesional_ and  R.tc_nombre_rol = 'Funcionario'  and C.tc_borrado = 0 
-            and UR.fk_id_rol = 3 and UR.fk_id_usuario = U.pk_cedula_usuario and C.fk_id_funcionario = U.pk_cedula_usuario);
-
+	 WHERE
+		/* join tables */
+		C.fk_id_funcionario = U.pk_cedula_usuario
+        AND U.pk_cedula_usuario = UR.fk_id_usuario
+        AND UR.fk_id_rol = 3
+        AND U.tn_borrado = 0
+        AND C.tc_borrado = 0
+        /* filters */
+        AND (id_funcionario_ = '' OR U.pk_cedula_usuario = id_funcionario_)
+        AND (id_cita_ = '' OR C.pk_id_cita = id_cita_)
+        AND (fecha_inicial_ = '' OR  C.tf_fecha >= fecha_inicial_)
+        AND (fecha_final_ = '' OR  C.tf_fecha <= fecha_final_)
+        AND (id_profesional_ = '' OR C.fk_id_profesional = id_profesional_)
+        AND (sexo_ = '' OR U.tc_sexo = sexo_);
 end;
 
 $$
