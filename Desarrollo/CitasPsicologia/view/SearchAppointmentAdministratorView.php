a8119c61fe9bc86ffae1583aace8eeab8e1774d5
YerlinLeal <yerlin.leal@ucr.ac.cr>
2021-02-03 02:44:48 -0600

    Validations SearchAppointmentAsministratorView

--- Desarrollo/CitasPsicologia/view/SearchAppointmentAdministratorView.php
+++ Desarrollo/CitasPsicologia/view/SearchAppointmentAdministratorView.php
@@ -1,4 +1,3 @@
-
 <?php
 include 'public/header.php';
 ?>
@@ -8,70 +7,86 @@ include 'public/header.php';
     <p class="titles"> Seleccione el tipo de b&uacutesqueda</p>
 </center>
 
-
-<div class="row">
-    <div class="col-sm ">
-        <div class="form-group row scheduleDatesFilter">
-            <label class="col-sm-4 control-label small" style="color: black" for="initialDate">Fecha inicial</label>
-            <input type="date" class="col-sm-4 form-control form-control-sm" id="initialDate" name="initialDate" />
-        </div>
-        <div class="form-group row scheduleDatesFilter">
-            <label class="col-sm-4 col-form-label-sm" style="color: black" for="identification">C&eacutedula</label>
-            <input type="number" class="col-sm-4 form-control form-control-sm"  id="identification" name="identification" />
-        </div>
-        <div class="form-group row scheduleDatesFilter">
-            <div class="col-sm ">
-                <div class="form-group row scheduleDatesFilter">
-                    <label class="col-sm-4 col-form-label-sm" style="color: black" for="gender">Sexo</label>
-                    <select class="col-sm-4 custom-select custom-select-sm"  id="gender" name="gender">
-                        <option value="">Seleccione una opción</option>
-                        <option value="M">M</option>
-                        <option value="F">F</option>
-                    </select>
-                </div>
+<div class="container text-left">
+    <div class="row">
+        <div class="col-sm-6" style="margin-top: 50px">
+            <!--FECHA INICIAL-->
+            <div class="form-group row scheduleDatesFilter">
+                <label class="col-sm-4 control-label small offset-sm-2" style="color: black" for="initialDate">Fecha inicial</label>
+                <input type="date" class="col-sm-4 form-control form-control-sm" id="initialDate" name="initialDate" onchange='alertInputInicialDate()' />
             </div>
+            
+            <!--IDENTIFICACION-->
+            <div class="form-group row scheduleDatesFilter">
+                <label class="col-sm-4 control-label small offset-sm-2" style="color: black" for="identification">C&eacutedula</label>
+                <input onkeypress="return onlyNumbers(event)" type="text" class="col-sm-4 form-control form-control-sm"  id="identification" name="identification" maxlength="9" />
+            </div>
+            
+            <!--SEXO-->
+            <div class="form-group row scheduleDatesFilter">
+                <label class="col-sm-4 control-label small offset-sm-2" style="color: black" for="gender">Sexo</label>
+                <select class="col-sm-4 custom-select custom-select-sm"  id="gender" name="gender">
+                    <option value="">Seleccione una opción</option>
+                    <option value="F">Femenino</option>
+                    <option value="M">Masculino</option>
+                </select>
+            </div> 
         </div>
-    </div>
-    <div class="col-sm ">
-        <div class="form-group row scheduleDatesFilter">
-            <label class="col-sm-4 control-label small" style="color: black" for="finalDate">Fecha Final</label>
-            <input type="date" class="col-sm-4 form-control form-control-sm" id="finalDate" name="finalDate" />
-        </div>
-        <div class="form-group row scheduleDatesFilter">
-            <label class="col-sm-4 col-form-label-sm" style="color: black" for="professional">Profesional</label>
-            <select  name= "professional" id="professional">
-                <option value="">Seleccione una opción</option>
-                <?php
-                foreach ($vars['professionals'] as $res) {
-                    ?>
+        
+        
+        <div class="col-sm-6" style="margin-top: 50px">
+            
+            <!--FECHA FINAL-->
+            <div class="form-group row scheduleDatesFilter">
 
-                    <option value="<?php echo $res['cedula']; ?>"><?php echo $res['nombre']; ?></option> 
+                <label class="col-sm-4 control-label small offset-sm-1" style="color: black" for="finalDate">Fecha Final</label>
+                <input type="date" class="col-sm-4 form-control form-control-sm" id="finalDate" name="finalDate" disabled="true" onchange='alertInputFinalDate()' />
+            </div>
+            
+            <!--PROFESIONAL-->
+            <div class="form-group row scheduleDatesFilter">
+                <label class="col-sm-4 control-label small offset-sm-1" style="color: black" for="professional">Profesional</label>
+                <select  class="col-sm-4 custom-select custom-select-sm" name= "professional" id="professional">
+                    <option value="">Seleccione una opción</option>
                     <?php
-                }
-                ?>
-
-            </select>  
+                    foreach ($vars['professionals'] as $res) {
+                        ?>
+                        <option value="<?php echo $res['cedula']; ?>"><?php echo $res['nombre']; ?></option> 
+                        <?php
+                    }
+                    ?>
+                </select>  
+            </div>
+            
+            <!--CONSECUTIVO-->
+            <div class="form-group row scheduleDatesFilter">
+                <label class="col-sm-4 control-label small offset-sm-1" style="color: black" for="consecutive">Consecutivo</label>
+                <input type="number" class="col-sm-4 form-control form-control-sm" id="consecutive" name="consecutive" />
+            </div>
+            
         </div>
-        <div class="form-group row scheduleDatesFilter">
-            <label class="col-sm-4 col-form-label-sm" style="color: black" for="consecutive">Consecutivo</label>
-            <input type="number" class="col-sm-4 form-control form-control-sm" id="consecutive" name="consecutive" />
-        </div>
+        
     </div>
-</div>
 
+</div>
 
 <div class="row">  
 
     <div class="col-sm " style="margin-top: 2em">
-
+        
+        <!--BT BUSCAR-->
         <input type="button" onclick="searchAppointmentByFilter($('#identification').val(), $('#consecutive').val(),
-                        $('#initialDate').val(), $('#finalDate').val(), $('#professional').val(), $('#gender').val())" class="btn btn-success btn-sm" id="btn-accept" value="Buscar"/>       
+            $('#initialDate').val(), $('#finalDate').val(), $('#professional').val(), $('#gender').val())" class="btn btn-success btn-sm" id="btn-accept" value="Buscar"/>       
+    
+        <!--BT CANCELAR-->
+        <button type="button" onclick="cleanFormConsultAppointment()" class="btn btn-success btn-sm" id="btn-cancel">Cancelar</button>
+
     </div>
 
 </div>
 
-<table id="tableID" class="table table-bordered table-striped table-hover table-sm table-responsive-md" style="margin: 2em">
-
+<div class="row d-flex justify-content-center">
+    <table id="tableID" class="table table-bordered table-striped table-hover table-sm table-responsive-md" style="margin: 2em">
     <thead>
         <tr>
             <th scope="col">Consecutivo</th>
@@ -107,8 +122,6 @@ include 'public/header.php';
                         Eliminar
                     </button>
                 </td>         
-                <td>               
-
             </tr>
 
             <?php
@@ -116,16 +129,19 @@ include 'public/header.php';
         ?>
     </tbody>
 </table>
-
+</div>
 
 <div class="" style="margin-left:50px">
     <span id="result"></span>
 </div>
+
 <!--BT ATRAS-->
 <div class = "col-sm-6">
     <a class = "btn btn-success btn-sm" href = "?controlador=User&accion=showAdministratorMainView">Atr&aacute;s</a>
 </div>
+
 <script src="public/js/Site.js" type="text/javascript"></script>
+
 <?php
 include_once 'public/footer.php';
 ?>
