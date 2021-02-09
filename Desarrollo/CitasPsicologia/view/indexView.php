12236f4af0de825db965c4d40fe44950da65cf02
Emily Melendez <emi.mci.95@gmail.com>
2021-02-09 15:45:54 -0600

    Fixed bugs about responsive

--- Desarrollo/CitasPsicologia/view/indexView.php
+++ Desarrollo/CitasPsicologia/view/indexView.php
@@ -1,10 +1,9 @@
+
 <?php
 include 'public/headerWithoutLogin.php';
 ?>
 
-
-
-<div class="row"s style="margin-top:  100px">
+<div class="row">
     <div class="col-4" style="margin-left: 50px">
         <div class="row homeOptions">
             <a class="btn btn-success col-md-5 btn-block" href="?controlador=User&accion=showLoginView">Iniciar Sesión</a>
@@ -31,7 +30,7 @@ include 'public/headerWithoutLogin.php';
 
     </div>
     <div class="col-7">
-        <div class="card dateRequest-card">
+        <div class="card dateRequest-card" style="margin-top: 20px">
             <div id="asignInstruction" class="card-header">
                 Asignación de citas por internet
             </div>
@@ -39,23 +38,12 @@ include 'public/headerWithoutLogin.php';
                 <br />
                 <br />               
                 <a class="btn btn-success" href="?controlador=User&accion=registerProfessional">Solicite su cita aquí</a>
-<!--                <button type="button"  name="userLoginbtn" id="clientLoginbtn" href="javascript:;"
-                        onclick="registerProfessional(304850984, 
-            123, 'Emily Profesional', 'Melendez', 
-            'Castro', '25348790',
-            '25678743', '1995-03-30', 'F', 
-            'soltera', 456,               
-            1, 'Marvin Melendez', 
-            88888888, 'secundaria', 
-            'psicologa',  'CPH78', 
-            'Cartago', 'Alvarado', 'Pacayas',
-            '800 metros este del BN');return false;" 
-                    class="btn btn-success">Solicite su cita aquí
-            </button>-->
+
             </div>
         </div>
     </div>
 </div>
+
 <?php
 include_once 'public/footer.php';
 ?>
