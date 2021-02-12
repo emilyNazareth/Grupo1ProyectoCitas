1999cba5d20b0836bae73537bfd85c4fbc75ecad
LuisHQ99 <luisca0699@gmail.com>
2021-02-12 11:35:53 -0600

    added test modify appointment

--- Desarrollo/CitasPsicologia/test/ModifyAppointmentTest.php
+++ Desarrollo/CitasPsicologia/test/ModifyAppointmentTest.php
@@ -8,15 +8,13 @@ class ModifyAppointmentTest
     public function testModifyAppointmentModel()
     {
         $model = AppointmentModel::singleton();
-
-        // $_POST['id_appointment'], $_POST['id_professional'], $_POST['date'], $_POST['hour'], $_POST['observations']
         $resultado =  $model->modify_appointment('', '', '', '', '');
         print_r($resultado);
     }
     public function testModifyAppointmentModel2()
     {
         $model = AppointmentModel::singleton();
-        $resultado =  $model->modify_appointment(1055, 306540789, "2021-02-10", "22:49", "modificando la cita");
+        $resultado =  $model->modify_appointment(1025, 301230321, "2021-02-10", "22:49", "modificando la cita luis");
         print_r($resultado);
     }
 }
