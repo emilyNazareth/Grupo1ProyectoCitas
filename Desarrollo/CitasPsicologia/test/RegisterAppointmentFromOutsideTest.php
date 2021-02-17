da7ac52cc62fcd099adddf5037c969864f4b9900
Ale Quesada <ale23200@gmail.com>
2021-02-16 18:07:58 -0600

    test a tareas GPC114, GPC116 y GPC122

---
+++ Desarrollo/CitasPsicologia/test/RegisterAppointmentFromOutsideTest.php
@@ -0,0 +1,29 @@
+<?php
+
+include 'controller/UserController.php';
+include  'model/AppointmentModel.php';
+
+class RegisterAppointmentOutsideTest {
+
+    public function testRegisterAppointmentOutside() {       
+        $model = AppointmentModel::singleton();
+
+        $resultado =  $model->register_appointment( 302900456, "2021-12-06", 
+                "9:30pm", "300000888", 'Test mismo cedu', "pendiente", 
+                "test BE emi", "test BE emi");            
+        print_r($resultado);
+    }
+
+    public function testRegisterFunctionaryOutside() {
+        $model = AppointmentModel::singleton();
+        $resultado = $model->register_functionary(
+                302900456, 'Test mismo cedu', "Ramirez", "Alvarado",
+                "85858585", "89898989", "1999-12-06", 'M', "Tecnico",
+                "Cartago", "Turri", "La Suiza", "Viudo(a)", "absd", "87878787",
+                "beto@gmail.com", "321", "2022-12-06", "nada", "nada",
+                "central", "2019-12-06");        
+        print_r($resultado);
+    }
+
+}
+
