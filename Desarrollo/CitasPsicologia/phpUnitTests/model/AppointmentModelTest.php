ef4a6c2fb1a01247b36dc469f2b54fd36e86e047
Emily Melendez <emi.mci.95@gmail.com>
2021-02-23 11:23:26 -0600

    testing merge

--- Desarrollo/CitasPsicologia/phpUnitTests/model/AppointmentModelTest.php
+++ Desarrollo/CitasPsicologia/phpUnitTests/model/AppointmentModelTest.php
@@ -1,7 +1,7 @@
 <?php 
 
 use PHPUnit\Framework\TestCase;
-//test merge two
+//test merge three
 include_once __DIR__ . '/../../model/AppointmentModel.php';
 
 class AppointmentModelTest extends TestCase {
